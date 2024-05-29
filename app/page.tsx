ddc22575b1d890be9776db75c82926d8938c6e46
judoree <135936771+judoree@users.noreply.github.com>
2024-05-29 20:12:37 +0900

    Responsive Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm dark:bg-gray-500">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-800 -mb-1 font-semibold  dark:text-gray-300">In transit</span>
-            <span className="text-4xl font-semibold  dark:text-white">Coolblue</span>
-          </div>
-          <div className="size-12 bg-orange-300 rounded-full" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2 py-1 transition text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 ">
-            Today
-          </span>
-          <span className=" dark:text-white">9:30~10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-500 absolute rounded-full w-full h-2" />
-          <div className="bg-green-500 absolute rounded-full w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between text-gray-500 mt-5 items-center  dark:text-white">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-300  dark:text-gray-420">Delivered</span>
-        </div>
+    <main className="bg-gray-400 h-screen flex items-center justify-center p-5 sm:bg-red-200  md:bg-green-200 lg:bg-cyan-300 xl:bg-orange-300">
+      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
+        <input
+          className="w-full bg-gray-500 rounded-full py-3 pl-5 outline-none ring ring-transparent transition-shadow focus:ring-offset-2 focus:ring-teal-400 "
+          type="text"
+          placeholder="Serch here"
+        />
+        <button className="bg-black text-white rounded-full py-2 bg-opacity-50 outline-none focus:scale-90  transition-transform font-medium md:px-10">
+          Secch
+        </button>
       </div>
     </main>
   );
