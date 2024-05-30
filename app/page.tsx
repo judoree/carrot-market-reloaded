425cc4e33a332c65fb9d5c2c45d72312d5f1a698
judoree <135936771+judoree@users.noreply.github.com>
2024-05-30 14:30:32 +0900

    List and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 export default function Home() {
   return (
     <main className="bg-gray-400 h-screen flex items-center justify-center p-5 sm:bg-red-200  md:bg-green-200 lg:bg-cyan-300 xl:bg-orange-300">
-      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-200 has-[:invalid]:ring">
-        <input
-          className="w-full bg-gray-500 rounded-full py-3 pl-5  ring ring-transparent transition-shadow focus:ring-offset-2 focus:ring-teal-400 invalid:focus:ring-red-500  peer: "
-          type="text"
-          required
-          placeholder="Email address"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">Email is required</span>
-        <button className=" text-white rounded-full py-2 bg-opacity-50  focus:scale-90  transition-transform font-medium md:px-10 bg-black peer-required:bg-green-500">
-          Log in
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+        {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+          <div key={index} className="flex items-center gap-5">
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping " />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
 }
+
+// odd ,even first last
