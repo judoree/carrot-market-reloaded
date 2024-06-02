9337c72ab7ba0e9fe25b945b456cbffa3cb3b3dd
judoree <135936771+judoree@users.noreply.github.com>
2024-06-02 21:18:56 +0900

    Route Handelers

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,12 +1,15 @@
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SoicalLogin from "@/components/social-login";
+import Link from "next/link";
 
 export default function CreateAccount() {
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col *:font-medium gap-2">
-        <h1 className="text-2xl">Wel come</h1>
+        <Link className="text-white hover:text-red-800" href="/">
+          <h1 className="text-2xl">Well come</h1>
+        </Link>
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form className="flex flex-col gap-3">
