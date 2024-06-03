894c45c94108434b7d4047cc108aa8b0fc5679a6
judoree <135936771+judoree@users.noreply.github.com>
2024-06-03 13:56:41 +0900

    useFormStatus

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,9 +5,9 @@ import SocialLogin from "@/components/social-login";
 export default function LogIn() {
   async function handlForm(formData: FormData) {
     "use server";
-    console.log(formData.get("email"), formData.get("password"));
-    console.log("I run in the server!");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
   }
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -17,7 +17,7 @@ export default function LogIn() {
       <form action={handlForm} className="flex flex-col gap-3">
         <FormInput name="email" type="email" placeholder="Email" required errors={[]} />
         <FormInput name="password" type="password" placeholder="Password" required errors={[]} />
-        <FormButton loading={false} text="Log in" />
+        <FormButton text="Log in" />
       </form>
 
       <SocialLogin />
