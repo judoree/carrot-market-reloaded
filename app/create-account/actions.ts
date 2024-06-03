6460e0ca4942a67fa828c16be712bb189fb98410
judoree <135936771+judoree@users.noreply.github.com>
2024-06-03 18:31:26 +0900

    Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,31 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+function checkUsername(username: string) {
+  return !username.includes("potato");
+}
+
+const checkPasswords = ({ password, confirm_password }: { password: string; confirm_password: string }) =>
+  password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username Must be a string",
+        required_error: "Where is my username???",
+      })
+      .min(3, "짧아")
+      .max(10, "길어")
+      .refine(checkUsername, "No potato allowed"),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(10, "비밀번호 가 너무 짧습니다 다시 설정 해주세요")
+      .max(15, "비밀번호 가 너무 길어요 다시 설정 해주세요"),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, { message: "Both passwords should be the same!", path: ["confirm_password"] });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -18,8 +18,8 @@ export default function CreateAccount() {
         <h2 className="text-xl">Fill in the form below to join!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <FormInput name="username" type="text" placeholder="Username" required errors={state?.fieldErrors.email} />
-        <FormInput name="email" type="email" placeholder="Email" required />
+        <FormInput name="username" type="text" placeholder="Username" required errors={state?.fieldErrors.username} />
+        <FormInput name="email" type="email" placeholder="Email" required errors={state?.fieldErrors.email} />
         <FormInput
           name="password"
           type="password"
