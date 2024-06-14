ab13d73552594c0a8f0d2e6c0b35d2b35fea8d33
judoree <135936771+judoree@users.noreply.github.com>
2024-06-14 17:16:21 +0900

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
 }
 
 export const config = {
